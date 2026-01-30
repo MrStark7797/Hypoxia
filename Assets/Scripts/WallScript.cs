41ee808dc095c852d8cd97040af7310131d6b4d7
djdodo2345 <omar.floodgate@googlemail.com>
2026-01-30 22:34:41 +0000

    yes

--- Assets/Scripts/WallScript.cs
+++ Assets/Scripts/WallScript.cs
@@ -1,8 +1,9 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class WallScript : MonoBehaviour
 {
-    
+    private GameObject[] tiles = new GameObject[48];
 
     public void AddRow(GameObject[] tiles)
     {
