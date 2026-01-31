7d9a12620dd7f64f26df90c5b686724df1b4f5b8
MrStark7797 <ws2g24@soton.ac.uk>
2026-01-31 00:29:58 +0000

    Working movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
 public class PlayerScript : MonoBehaviour
@@ -6,6 +7,7 @@ public class PlayerScript : MonoBehaviour
     public int oxygenLevel;
     public int pos = 0;
     public int lives = 5;
+    private SpriteRenderer spriteRenderer;
     public void moveLeft()
     {
         if (pos >= -2)
@@ -17,26 +19,45 @@ public class PlayerScript : MonoBehaviour
             else
             {
                 pos -= 1;
+                transform.position += new Vector3(-1,0,0);
+                oxygenLevel--;
+                Debug.Log("Moved Left. Position: " + pos);
             }
 
         }
     }
-
+    private void Start()
+    {
+        oxygenLevel = 10000000;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     public void moveRight()
     {
+        // Breadcrumb 1: Did we enter the function?
+        Debug.Log("Function called. Current pos: " + pos + " Oxygen: " + oxygenLevel);
+
         if (pos <= 2)
         {
             if (oxygenLevel <= 0)
             {
+                Debug.Log("Move failed: No oxygen!");
                 fall();
             }
             else
             {
                 pos += 1;
+                GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+                transform.position += new Vector3(0.5f, 0, 0);
+                GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+                transform.position += new Vector3(0.5f, 0, 0);
+                oxygenLevel--;
+                Debug.Log("SUCCESS! New pos: " + pos);
             }
-
         }
-        
+        else
+        {
+            Debug.Log("Move failed: Already at right boundary (pos > 2)");
+        }
     }
     public void moveUp()
     {
@@ -46,8 +67,12 @@ public class PlayerScript : MonoBehaviour
         }
         else
         {
-
+            oxygenLevel--;
+            GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
         }
+    }
+    public void moveDown() {
+        GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
     }
 
     public void Jump()
@@ -58,7 +83,8 @@ public class PlayerScript : MonoBehaviour
         }
         else
         {
-            
+            oxygenLevel-=3;
+            GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
         }
     }
 
@@ -71,8 +97,37 @@ public class PlayerScript : MonoBehaviour
         else
         {
 
+
         }
+    }
+    public void Update()
+    {
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            moveLeft();
+            Debug.Log(transform.position);
+            
+        }
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            moveUp();
+            Debug.Log(transform.position);
+        }
+        if (Keyboard.current.sKey.wasPressedThisFrame)
+        {
+            moveDown();
+            Debug.Log(transform.position);
+        }
+        if (Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            moveRight();
+            Debug.Log(transform.position);
+        }
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Jump();
+            Debug.Log(transform.position);
+        }
+    }
 
-
-}
 }
