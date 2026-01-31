04648ea2bcbf57c6af009d8a1c621f0dd427341d
MrStark7797 <ws2g24@soton.ac.uk>
2026-01-31 17:06:40 +0000

    AHhh

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,15 +1,27 @@
-﻿using TMPro;
+﻿using System.Collections;
+using System.Net;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
+using static UnityEngine.Rendering.DebugUI.Table;
 
 public class PlayerScript : MonoBehaviour
 {
     public GameObject camera;
     public int oxygenLevel;
-    public int pos = 0;
     public int lives = 5;
     public float speed;
+    public Vector3 currentCheckpoint;
+    public GameObject prefabPiton;
+    public Sprite perryClimb;
+    public Sprite perryFall;
+    private bool addRows = true;
+    private float peakPos = Mathf.Infinity;
+    private int moveCost;
+    public Light2D light2D;
+
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI o2Text;
     private SpriteRenderer spriteRenderer;
@@ -30,7 +42,7 @@ public class PlayerScript : MonoBehaviour
         if (direction == 1 || direction == 3)
         {
             var row = GameObject.Find("row" + position.y.ToString());
-            Debug.Log("Direction 1 or 3");
+            //Debug.Log("Direction 1 or 3");
             if (direction == 1)
             {
                 square = row.transform.GetChild((int)position.x + 3 + 1).gameObject;
@@ -60,7 +72,7 @@ public class PlayerScript : MonoBehaviour
     
     public void moveLeft()
     {
-        if (pos >= -2 && !isBoulder(transform.position, 3))
+        if (transform.position.x >= -2 && !isBoulder(transform.position, 3))
         {
             if (oxygenLevel <= 0)
             {
@@ -68,22 +80,26 @@ public class PlayerScript : MonoBehaviour
             }
             else
             {
-                pos -= 1;
+               
                 transform.position += new Vector3(-1, 0, 0);
-                oxygenLevel--;
-                Debug.Log("Moved Left. Position: " + pos);
+                oxygenLevel -= 1 * moveCost;
+                Debug.Log("Moved Left. Position: " + transform.position.x);
             }
 
         }
     }
     private void Start()
     {
+        lives = 5;
+        light2D = GetComponent<Light2D>();
+        light2D.enabled = false;
         oxygenLevel = 20;
         spriteRenderer = GetComponent<SpriteRenderer>();
         speed = 0.5f;
         UpdateOxygen("Oxygen: " + oxygenLevel.ToString());
-
-
+        currentCheckpoint = new Vector3(0, 0, -0.1f);
+        GameObject.Instantiate(prefabPiton, new Vector3(transform.position.x, transform.position.y + 0.25f, -0.05f), Quaternion.identity, GameObject.Find("row0").transform.GetChild((int)transform.position.x + 3));
+        moveCost = 1;
     }
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -91,7 +107,7 @@ public class PlayerScript : MonoBehaviour
         if (context.started)
         {
             Vector2 inputVec = context.ReadValue<Vector2>();
-            Debug.Log(inputVec);
+            //Debug.Log(inputVec);
             if (inputVec.x < -0.5f) moveLeft();
             else if (inputVec.x > 0.5f) moveRight();
 
@@ -100,14 +116,15 @@ public class PlayerScript : MonoBehaviour
             var row = GameObject.Find("row" + (transform.position.y).ToString());
             var square = row.transform.GetChild((int)transform.position.x + 3).gameObject;
             CheckForOxygen(square);
-
+            CheckCheckpoint(square);
+            CheckPeak();
             UpdateText("Level: " + transform.position.y.ToString());
             UpdateOxygen("Oxygen: " + oxygenLevel.ToString());
             if (square.name != "Rockface1_0(Clone)" && square.name != "Ice1_0(Clone)")
             {
-                Debug.Log(square.name);
+                //Debug.Log(square.name);
                 int randomInt = Random.Range(0, 5);
-                Debug.Log(randomInt);
+                //Debug.Log(randomInt);
                 if (square.name == "Rockface_boulder_0(Clone)" || square.name == "Ice_stalagmite_0(Clone)")
                 {
                     fall();
@@ -134,9 +151,12 @@ public class PlayerScript : MonoBehaviour
             UpdateOxygen("Oxygen: " + oxygenLevel.ToString());
             var row = GameObject.Find("row" + (transform.position.y).ToString());
             var square = row.transform.GetChild((int)transform.position.x + 3).gameObject;
+            CheckForOxygen(square);
+            CheckCheckpoint(square);
+            CheckPeak();
             if (square.name != "Rockface1_0(Clone)" && square.name != "Ice1_0(Clone)")
             {
-                Debug.Log(square.name);
+                //Debug.Log(square.name);
                 CheckForOxygen(square);
                 if (square.name == "Rockface_boulder_0(Clone)" || square.name == "Ice_stalagmite_0(Clone)")
                 {
@@ -152,26 +172,25 @@ public class PlayerScript : MonoBehaviour
     public void moveRight()
     {
         // Breadcrumb 1: Did we enter the function?
-        Debug.Log("Function called. Current pos: " + pos + " Oxygen: " + oxygenLevel);
+        Debug.Log("Function called. Current pos: " +transform.position+ " Oxygen: " + oxygenLevel);
 
-        if (pos <= 2 && !isBoulder(transform.position, 1))
+        if (transform.position.x <= 2 && !isBoulder(transform.position, 1))
         {
             if (oxygenLevel <= 0)
             {
-                Debug.Log("Move failed: No oxygen!");
+                //Debug.Log("Move failed: No oxygen!");
                 fall();
             }
             else
             {
-                pos += 1;
+
                 transform.position += new Vector3(1, 0, 0);
-                oxygenLevel--;
-                Debug.Log("SUCCESS! New pos: " + pos);
+                oxygenLevel -= 1 * moveCost;
             }
         }
         else
         {
-            Debug.Log("Move failed: Already at right boundary (pos > 2) OR Move into Boulder");
+            //Debug.Log("Move failed: Already at right boundary (pos > 2) OR Move into Boulder");
         }
     }
     public void moveUp()
@@ -182,20 +201,20 @@ public class PlayerScript : MonoBehaviour
         }
         else if (isBoulder(transform.position, 0))
         {
-            Debug.Log("Move is into Boulder");
+            //Debug.Log("Move is into Boulder");
         }
         else
         {
-            oxygenLevel--;
+            oxygenLevel -= 1 * moveCost;
             GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
             transform.position += new Vector3(0, 1, 0);
             camera.transform.position = new Vector3(0, transform.position.y, -10);
-            Debug.Log(camera.transform.position);
+            //Debug.Log(camera.transform.position);
 			// generate new terrain
 			GameObject rowCheck = GameObject.Find("row" + ((int)transform.position.y + 2).ToString());
 			GameObject wall = GameObject.Find("WallObject");
-			Debug.Log("Found wall " + wall.name); 
-			if (rowCheck == null && wall != null) {
+			//Debug.Log("Found wall " + wall.name); 
+			if (rowCheck == null && wall != null && addRows) {
 				wall.GetComponent<WallScript>().AddRow();
                
             }
@@ -205,7 +224,7 @@ public class PlayerScript : MonoBehaviour
     public void moveDown() {
         if (isBoulder(transform.position, 2))
         {
-            Debug.Log("Move is into Boulder");
+            //Debug.Log("Move is into Boulder");
         }
         else
         {
@@ -224,7 +243,7 @@ public class PlayerScript : MonoBehaviour
         } 
         else
         {
-            oxygenLevel-=3;
+            oxygenLevel-=3*moveCost;
             transform.position += new Vector3(0, 2, 0);
             GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
             camera.transform.position = new Vector3(0, transform.position.y, -10);
@@ -234,14 +253,14 @@ public class PlayerScript : MonoBehaviour
 			GameObject wall = GameObject.Find("WallObject");
 			rowPos = (int)transform.position.y + 2 - 1;
 			rowCheck = GameObject.Find("row" + rowPos.ToString());
-			Debug.Log("Found wall " + wall.name); 
-			if (rowCheck == null && wall != null) {
+			//Debug.Log("Found wall " + wall.name); 
+			if (rowCheck == null && wall != null && addRows) {
 				wall.GetComponent<WallScript>().AddRow(rowPos);
 			}
 			rowPos = (int)transform.position.y + 2;
 			rowCheck = GameObject.Find("row" + rowPos.ToString());
-			Debug.Log("Found wall " + wall.name); 
-			if (rowCheck == null && wall != null) {
+			//Debug.Log("Found wall " + wall.name); 
+			if (rowCheck == null && wall != null && addRows) {
 				wall.GetComponent<WallScript>().AddRow(rowPos);
 			}
         }
@@ -249,23 +268,41 @@ public class PlayerScript : MonoBehaviour
 
     public void fall()
     {
-        if (lives <= 1)
+        if (lives <= 1 || oxygenLevel <= 0)
         {
-
+            Debug.Log("AHHH");
+            SceneManager.LoadScene("Main");
         }
         else
         {
 
-
+            StartCoroutine(MoveToCheckpoint(currentCheckpoint));
+            //transform.position = currentCheckpoint;
+            //camera.transform.position = new Vector3(0, transform.position.y, -10);
         }
-        SceneManager.LoadScene("Main");
+        
     }
+
+    private IEnumerator MoveToCheckpoint(Vector3 aimPosition)
+    {
+        GetComponent<SpriteRenderer>().sprite = perryFall;
+        light2D.enabled = true;
+        while (transform.position != aimPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, aimPosition, Time.deltaTime);
+            camera.transform.position = new Vector3(0, transform.position.y, -10);
+            yield return new WaitForEndOfFrame();
+        }
+        light2D.enabled=false;
+        GetComponent<SpriteRenderer>().sprite = perryClimb;
+    }
+
     public void AddOxygen(float amount)
     {
         int oldLevel = oxygenLevel;
         oxygenLevel = Mathf.Min(100, oxygenLevel + (int)amount);
 
-        Debug.Log($"Oxygen Increased: {oldLevel} -> {oxygenLevel}");
+        //Debug.Log($"Oxygen Increased: {oldLevel} -> {oxygenLevel}");
         Canvas.ForceUpdateCanvases();
         UpdateOxygen("Oxygen: " + oxygenLevel.ToString());
         Canvas.ForceUpdateCanvases();
@@ -276,13 +313,40 @@ public class PlayerScript : MonoBehaviour
 
         if (oxygen != null)
         {
-            Debug.Log("Oxygen found on square!");
+            //Debug.Log("Oxygen found on square!");
             AddOxygen(oxygen.oxygenRestoreAmount);
             if (oxygen.prefabLooted != null)
             {
                 Instantiate(oxygen.prefabLooted, oxygen.transform.position, Quaternion.identity, oxygen.transform.parent);
             }
             Destroy(oxygen.gameObject);
+        }
+    }
+
+    private void CheckCheckpoint(GameObject square)
+    {
+        Debug.Log(transform.position);
+        Debug.Log(currentCheckpoint);
+        
+        if (transform.position.y - currentCheckpoint.y >= 10 && (square.name == "Rockface1_0(Clone)" || square.name == "Ice1_0(Clone)"))
+        {
+            if(transform.position.y % 50 < currentCheckpoint.y % 50) { moveCost++; }
+            currentCheckpoint = transform.position;
+            GameObject.Instantiate(prefabPiton, new Vector3(transform.position.x, transform.position.y+0.25f, -0.05f), Quaternion.identity, square.transform);
+        }
+    }
+
+    public void ReachPeak(float PeakPos)
+    {
+        addRows = false;
+        peakPos = PeakPos;
+    }
+
+    private void CheckPeak()
+    {
+        if (transform.position.y >= peakPos)
+        {
+            SceneManager.LoadScene("Main");
         }
     }
 }
